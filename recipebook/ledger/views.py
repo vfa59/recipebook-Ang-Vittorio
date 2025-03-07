756810ab46e1a40d20a19cb036c079d79ddcb8b0
vfa59 <vittorioang9898@gmail.com>
2025-03-07 11:59:25 +0800

    removed unnecessary imports

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponseNotFound
-from django.template import loader
 from .models import Recipe
 
 
