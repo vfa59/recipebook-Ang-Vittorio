dd2464e3c8636c15ae87d92569399504b1bad4b6
vfa59 <vittorioang9898@gmail.com>
2025-03-20 22:39:18 +0800

    Add login requirement for recipe views and create home view

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
+@login_required
+def home(request):
+    return render(request, 'base.html')
 
+@login_required
 def recipes_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes_list.html', {'recipes': recipes})
 
-
+@login_required
 def recipe_specific(request, num):   
     try:
         recipe = Recipe.objects.get(id=num)
