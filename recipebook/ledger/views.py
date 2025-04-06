a5ebd233c3478d9e1eb700ad4eb4f8fff3a60d96
vfa59 <vittorioang9898@gmail.com>
2025-04-07 00:56:25 +0800

    Updated views to include recipe creation and image upload

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseNotFound
 from django.contrib.auth.decorators import login_required
-from .models import Recipe
+from .models import Recipe, RecipeIngredient, RecipeImage
+from .forms import RecipeForm, RecipeIngredientForm, NewIngredientForm, RecipeImageForm
 
 @login_required
 def home(request):
@@ -21,3 +22,81 @@ def recipe_specific(request, num):
     
     ingredients = recipe.ingredients.all()
     return render(request, 'recipe_specific.html', {'recipe':recipe, 'ingredients': ingredients})
+
+@login_required
+def add_recipe(request):
+    if request.method == 'POST':
+        recipe_form = RecipeForm(request.POST)
+        ingredient_form = RecipeIngredientForm(request.POST)
+
+        if recipe_form.is_valid() and ingredient_form.is_valid():
+            recipe = recipe_form.save(commit=False)
+            recipe.author = request.user  
+            recipe.save()
+
+            ingredient = ingredient_form.save(commit=False)
+            ingredient.recipe = recipe  
+            ingredient.save()
+
+            return redirect('ledger:recipes_list')
+    
+    recipe_form = RecipeForm()
+    ingredient_form = RecipeIngredientForm()
+
+    return render(request, 'add_recipe.html', {'recipe_form': recipe_form, 'ingredient_form': ingredient_form,})
+
+@login_required
+def create_new_ingredient(request):
+    next_url = request.GET.get('next')
+
+    if request.method == 'POST':
+        new_ingredient_form = NewIngredientForm(request.POST)
+        if new_ingredient_form.is_valid():
+            new_ingredient_form.save()
+
+            if next_url:
+                return redirect(next_url)
+            else:
+                return redirect('ledger:add_recipe') 
+        
+    
+    new_ingredient_form = NewIngredientForm()
+
+    return render(request, 'new_ingredient.html', {'new_ingredient_form': new_ingredient_form})
+
+@login_required
+def add_recipe_ingredient(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+
+    if request.method == 'POST':
+        ingredient_form = RecipeIngredientForm(request.POST)
+
+        if ingredient_form.is_valid():
+            new_ingredient = ingredient_form.save(commit=False)
+            new_ingredient.recipe = recipe  
+            new_ingredient.save()  
+
+            return redirect('ledger:recipe_specific', num=recipe.pk) 
+
+    ingredient_form = RecipeIngredientForm()
+
+    return render(request, 'add_recipe_ingredient.html', {'ingredient_form': ingredient_form, 'recipe': recipe})
+
+
+@login_required
+def add_image(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+
+    if request.method == 'POST':
+        form = RecipeImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe_image = form.save(commit=False)
+            recipe_image.recipe = recipe  
+            recipe_image.save()
+            
+            return redirect('ledger:recipe_specific', num=recipe.pk)  
+    
+    form = RecipeImageForm()
+
+    return render(request, 'add_image.html', {'form': form, 'recipe': recipe})
+
